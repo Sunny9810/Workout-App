2739a51f0027d34a2bf8fbf5e3d2baf5513b8fc2
abigailmgarcia <abigailmg31@gmail.com>
2023-05-12 13:57:50 -0500

    array of quotes

---
+++ seeds/quotes.js
@@ -0,0 +1,32 @@
+const Quote =[
+     {
+        quote: "It's your workout, your body, your journey. OWN IT!",
+    },
+    {
+        quote: "When you feel like quitting, think about why you started.",
+    },
+    {
+        quote: "Mindset separates the best from the rest.",
+    },
+    {
+        quote: "Love yourself by loving your body",
+    },
+    {
+        quote: "Hard work + dedication = SUCCESS!",
+    },
+    {
+        quote: "All things are difficult before they are easy.",
+    },
+    {
+        quote: "It is about the journey not the destination.",
+    },
+    {
+        quote: "If you are going to quit anything, quit making excuses.",
+    },
+    {
+        quote: "Soreness is weakness leaving your body!",
+    },
+    {
+        quote: "Best views always come after the hardest climbs.",
+    },
+    ];
