bbe9598fdeb7eac6573e2d87b65992d9d9f40a19
abigailmgarcia <abigailmg31@gmail.com>
2023-05-17 15:17:42 -0500

    moved js work into different file

--- public/js/login.js
+++ public/js/login.js
@@ -22,32 +22,3 @@ const loginFormHandler = async (event) => {
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
-
-
-
-
-  if(window.matchMedia("(min-width: 576px)").matches){
-    var carouselWidth = $('.carousel-inner')[0].scrollWidth;
-  var cardWidth = $('.carousel-item').width();
-  
-  var scrollPosition = 0;
-  
-  $('.carousel-control-next').on('click', function(){
-    if(scrollPosition < (carouselWidth - (cardWidth *4))){
-      console.log('next');
-      scrollPosition = scrollPosition + cardWidth;
-      $('.carousel-inner').animate({scrollLeft:
-      scrollPosition},600);
-    }
-  });
-  $('.carousel-control-prev').on('click', function(){
-    if(scrollPosition > 0){
-      console.log('previous');
-      scrollPosition = scrollPosition - cardWidth;
-      $('.carousel-inner').animate({scrollLeft:
-      scrollPosition},600);
-    }
-  });
-  }else{
-    
-  }
