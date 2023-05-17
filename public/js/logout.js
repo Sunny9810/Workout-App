5e486f21916e6677226821a2eaa3a597035ec79a
abigailmgarcia <abigailmg31@gmail.com>
2023-05-17 15:07:56 -0500

    trying to find why my styling isn't working

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,17 +1,71 @@
-// const logout = async () => {
-//   const response = await fetch('/api/users/logout', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//   });
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-//   if (response.ok) {
-//     document.location.replace('/login');
-//   } else {
-//     alert(response.statusText);
-//   }
-// };
+  if (response.ok) {
+    document.location.replace('/login');
+  } else {
+    alert(response.statusText);
+  }
+};
 
-// document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector('#logout').addEventListener('click', logout);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Abigail's work section 
+
 
 // IF we are using jQUery
 // if(window.matchMedia("(min-width: 576px)").matches){
