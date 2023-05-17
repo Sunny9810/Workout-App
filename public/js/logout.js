37c18c9313d929a19cbd57106a248159ebb0c01b
abigailmgarcia <abigailmg31@gmail.com>
2023-05-17 14:11:13 -0500

    added on click function for next&previous button

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,35 @@
-const logout = async () => {
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
+// const logout = async () => {
+//   const response = await fetch('/api/users/logout', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//   });
 
-  if (response.ok) {
-    document.location.replace('/login');
-  } else {
-    alert(response.statusText);
+//   if (response.ok) {
+//     document.location.replace('/login');
+//   } else {
+//     alert(response.statusText);
+//   }
+// };
+
+// document.querySelector('#logout').addEventListener('click', logout);
+var carouselWidth = $('.carousel-inner')[0].scrollWidth;
+var cardWidth = $('.carousel-item').width();
+
+var scrollPosition = 0;
+
+$('.carousel-control-next').on('click', function(){
+  if(scrollPosition < (carouselWidth - (cardWidth *4))){
+    console.log('next');
+    scrollPosition = scrollPosition + cardWidth;
+    $('.carousel-inner').animate({scrollLeft:
+    scrollPosition},600);
   }
-};
-
-document.querySelector('#logout').addEventListener('click', logout);
+});
+$('.carousel-control-prev').on('click', function(){
+  if(scrollPosition > 0){
+    console.log('previous');
+    scrollPosition = scrollPosition - cardWidth;
+    $('.carousel-inner').animate({scrollLeft:
+    scrollPosition},600);
+  }
+});
