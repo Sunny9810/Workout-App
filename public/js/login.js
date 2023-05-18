16344a8a8a7109b1b0adec8690900417096df6e8
Oscar Leal <oscarleal234@gmail.com>
2023-05-18 01:45:00 -0500

    fixed the login to be able to authenticate users

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,61 @@
+// const loginFormHandler = async (event) => {
+//   event.preventDefault();
+
+//   const email = document.querySelector("#email-login").value.trim();
+//   const password = document.querySelector("#password-login").value.trim();
+//   console.log(email, password);
+//   if (email && password) {
+//     const response = await fetch("/api/users/login", {
+//       method: "GET",
+//       // body: JSON.stringify({ email, password }),
+//       // headers: { "Content-Type": "application/json" },
+//     });
+//     console.log(response);
+//     if (response.ok) {
+//       document.location.replace("/workoutpage");
+//     } else {
+//       alert("Failed to log in");
+//     }
+//   }
+//   console.log("login form");
+// };
+// console.log("hello");
+// document
+//   .querySelector(".login-form")
+//   .addEventListener("submit", loginFormHandler);
+
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-console.log(email, password)
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  console.log(email, password);
+
   if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-console.log(response)
-    // if (response.ok) {
-    //   document.location.replace('/');
-    // } else {
-    //   alert('Failed to log in');
-    // }
+    try {
+      const response = await fetch("/api/users/login", {
+        method: "POST", // Use the POST method instead of GET for sending sensitive data like passwords
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        // Redirect to the desired page after successful login
+        document.location.replace("/workout");
+      } else {
+        // Display an error message if login fails
+        alert("Failed to log in");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("An error occurred during login. Please try again."); // Handle any network or server errors
+    }
+  } else {
+    alert("Please enter both email and password."); // Handle empty email or password fields
   }
-console.log("login form")
 };
-console.log("hello")
+
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
