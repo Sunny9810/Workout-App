fb7b3bf052daa289413659ce8cf813287405087f
abigailmgarcia <abigailmg31@gmail.com>
2023-05-20 15:19:07 -0500

    fixed conflict

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const { User } = require('../models');
 const withAuth = require('../utils/auth');
 const { Quotes } = require('../models');
 const { User, MuscleGroup, Exercises, Quotes } = require("../models");
-const withAuth = require("../utils/auth");
 const { route } = require("./api");
 
 // !!!!!!!!!!!!!!!!!!LOGIN ROUTES !!!!!!!!!!!!!!!! //
